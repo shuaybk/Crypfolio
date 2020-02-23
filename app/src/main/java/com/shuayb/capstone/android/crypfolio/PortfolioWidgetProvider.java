a0c0c13173559dcf4677b955b518940fd538b702
Shuayb Khan <shuaybk@gmail.com>
2020-02-23 15:44:38 -0500

    Create basic widget (no real functionality)

---
+++ app/src/main/java/com/shuayb/capstone/android/crypfolio/PortfolioWidgetProvider.java
@@ -0,0 +1,30 @@
+package com.shuayb.capstone.android.crypfolio;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class PortfolioWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.portfolio_widget);
+            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+}
+
